7836029ecbbfc53dfddc50b26b6b20ae93ce6c98
Crypto-V <58492579+Crypto-V@users.noreply.github.com>
2021-02-12 21:36:47 +0000

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Checking connection with the repository");
+        System.out.println("Adding code from GitHub");
     }
 }
